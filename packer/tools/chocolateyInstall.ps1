72b4b2ccc807d4db8844ad6f6a6a51c5ce446b37
Stefan Scherer <scherer_stefan@icloud.com>
2013-12-13 18:19:03 +0100

    packer 0.4.1

--- packer/tools/chocolateyInstall.ps1
+++ packer/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-$url = 'https://dl.bintray.com/mitchellh/packer/0.4.0_windows_386.zip'
-$url64bit = 'https://dl.bintray.com/mitchellh/packer/0.4.0_windows_amd64.zip'
+$url = 'https://dl.bintray.com/mitchellh/packer/0.4.1_windows_386.zip'
+$url64bit = 'https://dl.bintray.com/mitchellh/packer/0.4.1_windows_amd64.zip'
 $unzipLocation = "$env:SystemDrive\HashiCorp\packer"
 
 Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit"
