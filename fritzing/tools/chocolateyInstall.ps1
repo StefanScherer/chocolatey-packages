f46523e434310b6a4b6c15260b07a8c5a9af42b8
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-04 13:02:06 +0100

    improved code from already existing package

--- fritzing/tools/chocolateyInstall.ps1
+++ fritzing/tools/chocolateyInstall.ps1
@@ -1,5 +1,15 @@
-$url = 'http://fritzing.org/download/0.8.5b/windows/fritzing.2013.12.17.pc.zip'
-$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+try {
+	$packageName = 'fritzing'
+    $url = 'http://fritzing.org/download/0.8.5b/windows/fritzing.2013.12.17.pc.zip'
+			 
+	$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+    Install-ChocolateyZipPackage $packageName $url $unzipLocation
 
-Install-ChocolateyZipPackage "fritzing" "$url" "$unzipLocation" 
-Install-ChocolateyDesktopLink "$unzipLocation\fritzing.2013.12.17.pc\fritzing.exe"
+    $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\fritzing.2013.12.17.pc\fritzing.exe"
+    Install-ChocolateyDesktopLink $targetFilePath
+
+    Write-ChocolateySuccess $packageName
+}   catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
--- fritzing/tools/chocolateyUninstall.ps1
+++ fritzing/tools/chocolateyUninstall.ps1
@@ -1,6 +1,10 @@
-#$url = 'http://fritzing.org/download/0.8.5b/windows/fritzing.2013.12.17.pc.zip'
-#$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'fritzing'
 
-remove-item "$env:UserProfile/Desktop/fritzing.exe.lnk"  -ErrorAction SilentlyContinue
-
-# UnInstall-ChocolateyZipPackage "fritzing" "$url"
+try {
+  Remove-Item "$Home\Desktop\fritzing.exe.lnk"
+  # the following is all part of error handling
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
