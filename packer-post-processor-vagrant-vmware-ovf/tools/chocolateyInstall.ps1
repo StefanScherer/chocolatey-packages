66f50560d14b7c49deaa97b2775e897089f9a43d
Stefan Scherer <scherer_stefan@icloud.com>
2014-06-16 03:18:45 -0700

    added packer-post-processor-vagrant-vmware-ovf

--- packer-post-processor-vagrant-vmware-ovf/tools/chocolateyInstall.ps1
+++ packer-post-processor-vagrant-vmware-ovf/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ $url = 'https://github.com/gosddc/packer-post-processor-vagrant-vmware-ovf/relea
 $url64bit = 'https://github.com/gosddc/packer-post-processor-vagrant-vmware-ovf/releases/download/v0.1.2/packer-post-processor-vagrant-vmware-ovf.windows-amd64.zip'
 $unzipLocation = "$env:SystemDrive\HashiCorp\packer"
 
-Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit"
+Install-ChocolateyZipPackage "packer-post-processor-vagrant-vmware-ovf" "$url" "$unzipLocation" "$url64bit"
 Install-ChocolateyPath $unzipLocation
