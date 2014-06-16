9f48eb1455756fec550cf7695872807f20102859
Stefan Scherer <scherer_stefan@icloud.com>
2014-06-16 06:29:46 -0700

    write .packerconfig

--- packer-post-processor-vagrant-vmware-ovf/tools/chocolateyInstall.ps1
+++ packer-post-processor-vagrant-vmware-ovf/tools/chocolateyInstall.ps1
@@ -4,3 +4,23 @@ $unzipLocation = "$env:SystemDrive\HashiCorp\packer"
 
 Install-ChocolateyZipPackage "packer-post-processor-vagrant-vmware-ovf" "$url" "$unzipLocation" "$url64bit"
 Install-ChocolateyPath $unzipLocation
+
+
+# install .packerconfig 
+# TODO: if already exists, append the post-processor to .packerconfig
+$packerConfig = "$env:USERPROFILE\.packerconfig"
+
+$json = @"
+{
+  "post-processors": {
+      "vagrant-vmware-ovf": "packer-post-processor-vagrant-vmware-ovf"
+  }
+}
+"@
+
+if (!(Test-Path $packerConfig)) {
+  Set-Content -path $packerConfig -value $json
+  Write-Host "Creating .packerconfig"
+} else {
+  Write-Host "Please update your .packerconfig manually"
+}
