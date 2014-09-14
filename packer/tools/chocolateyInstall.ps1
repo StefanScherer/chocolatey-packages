0aa656e86022f7a076bdc6a1bace2a4369df8bc7
Stefan Scherer <scherer_stefan@icloud.com>
2014-09-14 10:10:17 +0200

    fixed packer 0.7.1, copy packer-packer.exe to packer.exe

--- packer/tools/chocolateyInstall.ps1
+++ packer/tools/chocolateyInstall.ps1
@@ -1,7 +1,25 @@
-$url = 'https://dl.bintray.com/mitchellh/packer/0.7.0_windows_386.zip'
-$url64bit = 'https://dl.bintray.com/mitchellh/packer/0.7.0_windows_amd64.zip'
+$url = 'https://dl.bintray.com/mitchellh/packer/0.7.1_windows_386.zip'
+$url64bit = 'https://dl.bintray.com/mitchellh/packer/0.7.1_windows_amd64.zip'
 $unzipLocation = "$env:SystemDrive\HashiCorp\packer"
+
+$packerExe = $unzipLocation + "\packer.exe"
+$packerPackerExe = $unzipLocation + "\packer-packer.exe"
+
+If (Test-Path $packerExe) {
+  Write-Host "Removing old $packerExe"
+  Remove-Item $packerExe
+}
+If (Test-Path $packerPackerExe) {
+  Write-Host "Removing old $packerPackerExe"
+  Remove-Item "$packerPackerExe"
+}
 
 Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit"
 Install-ChocolateyPath $unzipLocation
 
+If (-Not (Test-Path $packerExe)) {
+  If (Test-Path "$packerPackerExe") {
+    Write-Host "Copying $packerPackerExe to packer.exe"
+    Copy-Item -Path "$packerPackerExe" -Destination $packerExe
+  }
+}
