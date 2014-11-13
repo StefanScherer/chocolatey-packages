0b6c716e8178c529f49a2bf64d83e34505e2a622
Stefan Scherer <scherer_stefan@icloud.com>
2014-11-13 22:02:08 +0100

    post-processor 0.2.1 works with packer 0.7.x

---
+++ vs2013u4_community/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+  'vs_community.exe' 'exe' "/Passive /NoRestart /Log $($env:temp)\vs_community.log" `
+  'http://download.microsoft.com/download/7/1/B/71BA74D8-B9A0-4E6C-9159-A8335D54437E/vs_community.exe' -validExitCodes @(0,3010)
