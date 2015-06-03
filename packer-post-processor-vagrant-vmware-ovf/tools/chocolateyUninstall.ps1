b853e7b5502d585ffedd63c5cd95530277b1e541
Stefan Scherer <scherer_stefan@icloud.com>
2015-06-03 23:04:17 +0200

    install packer plugin in package dir

--- packer-post-processor-vagrant-vmware-ovf/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-$unzipLocation = "$env:SystemDrive\HashiCorp\packer"
-$pluginExe = $unzipLocation + "\packer-post-processor-vagrant-vmware-ovf.exe"
-
-if (Test-Path $pluginExe) {
-  Remove-Item $pluginExe
-}
