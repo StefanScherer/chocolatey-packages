7a93b07a6ee4091f8da9d5517ec73a260b87afec
Stefan Scherer <scherer_stefan@icloud.com>
2015-06-11 23:02:00 +0200

    update to 0.8.0 and waiting for new zip files to update checksum as well

--- packer/tools/chocolateyInstall.ps1
+++ packer/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.7.5_windows_386.zip'
+$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.0_windows_386.zip'
 $checksum = '4223a8e76eed0f8ad22a24b68495c37d85a334c7'
-$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.7.5_windows_amd64.zip'
+$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.0_windows_amd64.zip'
 $checksum64 = '4d5db9c33e471729561cd9a83ae3024b34b2d934'
 $legacyLocation = "$env:SystemDrive\HashiCorp\packer"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
