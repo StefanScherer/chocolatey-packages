0b89f0198cb436f7d1322ba1a9e17fefb44ca34c
Stefan Scherer <scherer_stefan@icloud.com>
2015-08-24 16:12:14 +0200

    Update packer 0.8.6

--- packer/tools/chocolateyInstall.ps1
+++ packer/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.5_windows_386.zip'
-$checksum = 'a3215ab245137fdd8a097612b85fe2deedcba93f'
-$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.5_windows_amd64.zip'
-$checksum64 = '5b04f6a04b7068a075e4238f4d04aa79e57613ee'
+$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.6_windows_386.zip'
+$checksum = '8d0bd037909206926d988b30e9336faf105dffe97c2924d455b28de437557c7f'
+$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.6_windows_amd64.zip'
+$checksum64 = '786503f2ffe658c1b318af227eabb8c10f3f425608ad4ef709206757931b7eee'
 $legacyLocation = "$env:SystemDrive\HashiCorp\packer"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit" `
- -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
+ -checksum $checksum -checksumType 'sha256' -checksum64 $checksum64
 
 If (Test-Path $legacyLocation) {
   Write-Host "Removing old packer installation from $legacyLocation"
