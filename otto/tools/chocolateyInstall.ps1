961d5dcdcb83483609dfe1ed954c74e3ce5b8883
Stefan Scherer <scherer_stefan@icloud.com>
2015-09-28 21:17:33 +0200

    Switch to SHA1

--- otto/tools/chocolateyInstall.ps1
+++ otto/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $url = 'https://dl.bintray.com/mitchellh/otto/otto_0.1.0_windows_386.zip'
-$checksum = '0df2fc8c69b27f08bd03e98dcbbf45f5941121a0769b9ac253f201c9c314ff71'
+$checksum = 'c0949e03eae0f853a4cea62556202664110265bd'
 $url64bit = 'https://dl.bintray.com/mitchellh/otto/otto_0.1.0_windows_amd64.zip'
-$checksum64 = '8aa135bd40db9211015626a618b8aaae5487b28eb5fba04a04d5eedfa14fd177'
+$checksum64 = '9c536d080db8908d22a30ab444de8410269b07f1'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage "otto" "$url" "$unzipLocation" "$url64bit" `
- -checksum $checksum -checksumType 'sha256' -checksum64 $checksum64
+ -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
