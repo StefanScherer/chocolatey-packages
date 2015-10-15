7b03601ad1e53ec68924ed6db4aa7878bb275c35
Stefan Scherer <scherer_stefan@icloud.com>
2015-10-15 08:40:33 +0200

    docker-compose 1.5.0rc1

--- docker-compose/tools/chocolateyInstall.ps1
+++ docker-compose/tools/chocolateyInstall.ps1
@@ -4,22 +4,6 @@ $url64 = $url
 $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $file = "$($destination)\docker-compose.exe"
 
-if ($packageName) {
-  $packagelibPath=$env:chocolateyPackageFolder
-  if (!(Test-Path -path $packagelibPath)) {
-    New-Item $packagelibPath -type directory
-  }
-}
-
 if (![System.IO.Directory]::Exists($destination)) {[System.IO.Directory]::CreateDirectory($destination)}
 
-Update-SessionEnvironment
-
-try {
-  Get-ChocolateyWebFile $packageName $file $url $url64bit
-
-  Write-ChocolateySuccess $packageName
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
-}
+Get-ChocolateyWebFile $packageName $file $url $url64bit
