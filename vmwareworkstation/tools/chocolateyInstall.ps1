95519764b9c99e60467a9f89fabff3ce764c13c7
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-17 17:43:05 +0200

    Fix unattended installation

--- vmwareworkstation/tools/chocolateyInstall.ps1
+++ vmwareworkstation/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $url64 = $url
 $checksum = 'b8096e025e30e7015711b83644e851207a0f1d82'
 $checksum64 = $checksum
 $checksumType = 'sha1'
-$silentArgs = '/s /nsr /v EULAS_AGREED=1'
+$silentArgs = '/s /v/qn EULAS_AGREED=1'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64
