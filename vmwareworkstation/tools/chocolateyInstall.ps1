7d905afebec6999ee15dd63973d06c133fd51429
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-31 09:51:36 +0100

    Update VMware Workstation 12.5.1

--- vmwareworkstation/tools/chocolateyInstall.ps1
+++ vmwareworkstation/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'vmwareworkstation'
 $installerType = 'exe'
-$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.0-4352439.exe'
+$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.1-4542065.exe'
 $url64 = $url
-$checksum = 'bacaa3b74484dea9fdaf7bd5ad02935c90841632'
+$checksum = 'e404617222a58370ea39ec110b7341e66da3ae7d'
 $checksum64 = $checksum
 $checksumType = 'sha1'
 $silentArgs = '/s /v/qn EULAS_AGREED=1'
