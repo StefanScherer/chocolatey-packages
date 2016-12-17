4b696c0f5e28651d9bf3400f4f8362e0d11354c7
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-17 21:24:10 +0100

    Update VMware Workstation Pro 12.5.2

--- vmwareworkstation/tools/chocolateyInstall.ps1
+++ vmwareworkstation/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'vmwareworkstation'
 $installerType = 'exe'
-$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.1-4542065.exe'
+$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.2-4638234.exe'
 $url64 = $url
-$checksum = 'e404617222a58370ea39ec110b7341e66da3ae7d'
+$checksum = '512ccf2a89b74fb05807928fbf063819888ec492'
 $checksum64 = $checksum
 $checksumType = 'sha1'
 $silentArgs = '/s /v/qn EULAS_AGREED=1'
