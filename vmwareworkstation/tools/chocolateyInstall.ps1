fdeba4d9f2450c465fef872720de6a9ff46cb09b
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-22 15:29:48 +0200

    Update VMware Workstation 14.1.2

--- vmwareworkstation/tools/chocolateyInstall.ps1
+++ vmwareworkstation/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'vmwareworkstation'
 $installerType = 'exe'
-$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-14.1.1-7528167.exe'
+$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-14.1.2-8497320.exe'
 $url64 = $url
-$checksum = '2d360e5f90cdbef9cbd1ec598b99890dfc26a06d7227bcbc30da1ec635d055e8'
+$checksum = '33caa5ddac80f75dfd17215eeafab46856dfa7f45219260f7663f4a583cfd0c0'
 $checksum64 = $checksum
 $checksumType = 'sha256'
 $silentArgs = '/s /v/qn EULAS_AGREED=1'
